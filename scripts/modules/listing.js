527f82b1a70669862e4c8337cccd16dc817369a4
neojato <kpaul86@gmail.com>
2015-01-17 12:35:54 -0600

    Step 3: Add a controller

--- scripts/modules/listing.js
+++ scripts/modules/listing.js
@@ -1,1 +1,5 @@
-angular.module('listing.module', []);
+angular.module('listing.module', [])
+  .controller('listingCtrl', ['$scope', function($scope) {
+    'use strict';
+    $scope.title = 'Technology News';
+  }]);
