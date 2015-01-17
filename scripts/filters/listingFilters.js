827fa50e31839c501254831cfc766aa38466bf36
neojato <kpaul86@gmail.com>
2015-01-17 15:47:15 -0600

    Step 14:  Use angular.forEach

--- scripts/filters/listingFilters.js
+++ scripts/filters/listingFilters.js
@@ -1,6 +1,9 @@
 angular.module('listing.filters', [])
   .filter('newsFilter', function() {
     return function(items, filters) {
-
+      angular.forEach(items, function(item) {
+        console.log(item);
+      });
+      return items;
     };
   });
