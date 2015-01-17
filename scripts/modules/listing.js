76bbdacd812eb67c516f9421f6c9104f61082fe1
neojato <kpaul86@gmail.com>
2015-01-17 13:35:46 -0600

    Step 6: Add your service

--- scripts/modules/listing.js
+++ scripts/modules/listing.js
@@ -1,5 +1,9 @@
-angular.module('listing.module', [])
-  .controller('listingCtrl', ['$scope', function($scope) {
+angular.module('listing.module', ['listing.services'])
+  .controller('listingCtrl', ['$scope', 'data', function($scope, data) {
     'use strict';
     $scope.title = 'Technology News';
+    $scope.setData = function(data) {
+      $scope.articles = data.articles;
+      console.log($scope.articles);
+    }
   }]);
