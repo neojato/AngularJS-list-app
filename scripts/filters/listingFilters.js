6aec08cc16da91353d7b340a61e42f2a869dc669
neojato <kpaul86@gmail.com>
2015-01-17 17:42:25 -0600

    Step 17: Filter for tags

--- scripts/filters/listingFilters.js
+++ scripts/filters/listingFilters.js
@@ -4,7 +4,18 @@ angular.module('listing.filters', [])
       var filtered = [];
       angular.forEach(items, function(item) {
         var matchText = filters.query == "" || item.title.indexOf(filters.query) > -1 || item.description.indexOf(filters.query) > -1;
-        if(matchText) {
+        var selectedTags = 0;
+        var tagsMatched = 0;
+        angular.forEach(filters.tags, function (tag) {
+          if (tag.selected) {
+            selectedTags += 1;
+            if (item.tags.indexOf(tag.label) > -1) {
+              tagsMatched += 1;
+            }
+          };
+        });
+        var matchAnyTag = selectedTags == 0 || tagsMatched > 0;
+        if(matchText && matchAnyTag) {
           filtered.push(item);
         }
       });
