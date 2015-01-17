e4339daf3ebfe74598d4f738e34f03a2783772fa
neojato <kpaul86@gmail.com>
2015-01-17 17:36:48 -0600

    Step 16: Filter for text

--- scripts/filters/listingFilters.js
+++ scripts/filters/listingFilters.js
@@ -1,9 +1,13 @@
 angular.module('listing.filters', [])
   .filter('newsFilter', function() {
     return function(items, filters) {
+      var filtered = [];
       angular.forEach(items, function(item) {
-        console.log(item);
+        var matchText = filters.query == "" || item.title.indexOf(filters.query) > -1 || item.description.indexOf(filters.query) > -1;
+        if(matchText) {
+          filtered.push(item);
+        }
       });
-      return items;
+      return filtered;
     };
   });
